0cb4889665b535a8626ae51775fb524384e98b0e
Guest User <Guest@epicodus-107.local>
2018-01-03 17:43:02 -0800

    updates category controller

---
+++ ToDoList/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoList.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+
+		public IActionResult Index()
+		{
+			return View(db.Categories.ToList());
+		}
+    }
+}
