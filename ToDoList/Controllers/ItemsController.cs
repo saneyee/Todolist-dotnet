58971d7525aff6b7d3c008e4163b638aea14529f
Guest User <Guest@epicodus-107.local>
2018-01-03 17:22:41 -0800

    the items works with categories

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,76 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
-using System.Linq;
-using System.Collections.Generic;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
 	public class ItemsController : Controller
 	{
 		private ToDoListContext db = new ToDoListContext();
-
 		public IActionResult Index()
 		{
 			return View(db.Items.Include(items => items.Category).ToList());
 		}
 
-		//public IActionResult Details(int id)
-		//{
-		//	Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-		//	return View(thisItem);
-		//}
+		public IActionResult Details(int id)
+		{
+			var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+			return View(thisItem);
+		}
 
-		//public IActionResult Create()
-		//{
-		//	return View();
-		//}
+		public IActionResult Create()
+		{
+			ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+			return View();
+		}
 
-		//[HttpPost]
-		//public IActionResult Create(Item item)
-		//{
-		//	db.Items.Add(item);
-		//	db.SaveChanges();
-		//	return RedirectToAction("Index");
-		//}
+		[HttpPost]
+		public IActionResult Create(Item item)
+		{
+			db.Items.Add(item);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 
-		//public IActionResult Edit(int id)
-		//{
-		//	var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-		//	return View(thisItem);
-		//}
+		public IActionResult Edit(int id)
+		{
+			var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+			ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "Name");
+			return View(thisItem);
+		}
 
-		//[HttpPost]
-		//public IActionResult Edit(Item item)
-		//{
-		//	db.Entry(item).State = EntityState.Modified;
-		//	db.SaveChanges();
-		//	return RedirectToAction("Index");
-		//}
+		[HttpPost]
+		public IActionResult Edit(Item item)
+		{
+			db.Entry(item).State = EntityState.Modified;
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 
+		public ActionResult Delete(int id)
+		{
+			var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+			return View(thisItem);
+		}
 
-		//public IActionResult Delete(int id)
-		//{
-		//	var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-		//	return View(thisItem);
-		//}
-
-		//[HttpPost, ActionName("Delete")]
-		//public IActionResult DeleteConfirmed(int id)
-		//{
-		//	var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
-		//	db.Items.Remove(thisItem);
-		//	db.SaveChanges();
-		//	return RedirectToAction("Index");
-		//}
-
-
-
-
-
-
-
-
-
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeleteConfirmed(int id)
+		{
+			var thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+			db.Items.Remove(thisItem);
+			db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 	}
 }
