0b83b8b45011ad799cf4753942557edf41b87f90
Guest User <Guest@epicodus-107.local>
2018-01-04 13:33:51 -0800

    db added

--- todolist.sql
+++ todolist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 04, 2018 at 01:54 AM
+-- Generation Time: Jan 04, 2018 at 10:32 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -39,7 +39,8 @@ CREATE TABLE `Categories` (
 
 INSERT INTO `Categories` (`CategoryId`, `Name`) VALUES
 (1, 'Home Works'),
-(2, 'Outside Works');
+(2, 'Outside Works'),
+(4, 'Home Chores');
 
 -- --------------------------------------------------------
 
@@ -58,11 +59,11 @@ CREATE TABLE `Items` (
 --
 
 INSERT INTO `Items` (`ItemId`, `Description`, `CategoryId`) VALUES
-(1, 'Learn C#  today', 1),
 (2, ' Learn .NET  ', 1),
-(3, 'Conquer the internet', 1),
-(4, 'Make to do list ', 1),
-(5, ' Walk the dog ', 2);
+(3, 'Conquer the internet', 2),
+(5, ' Walk the dog !!!', 2),
+(8, 'Learn C#  today!', 1),
+(9, 'Clean House', 4);
 
 --
 -- Indexes for dumped tables
@@ -90,12 +91,12 @@ ALTER TABLE `Items`
 -- AUTO_INCREMENT for table `Categories`
 --
 ALTER TABLE `Categories`
-  MODIFY `CategoryId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `CategoryId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `Items`
 --
 ALTER TABLE `Items`
-  MODIFY `ItemId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ItemId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
